142ea2f8eaedc5bd25b71e2c795aa7c3c35c100c
Ian Tumulak <edden87@gmail.com>
2024-08-10 23:16:15 +0800

    Move main content in the Home component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,13 @@
-import { useEffect } from "react";
-import { Container, Grow, Grid } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { CircularProgress } from "@mui/material";
-import { fetchPostsApi } from "./api";
+import { Container } from "@mui/material";
 import "./index.css";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
 import NavBar from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home";
 
 function App() {
-    const dispatch = useDispatch();
-    const posts = useSelector(state => state.posts.items)
-
-    useEffect(() => {
-        dispatch(fetchPostsApi())
-    }, [dispatch])
-
     return (
         <Container maxWidth="lg" className="!flex flex-col gap-y-10">
             <NavBar/>
-            <Grow in>
-                <Container>
-                    <Grid minHeight={600} container justifyItems="space-between" alignItems="stretch" spacing={3}>
-                    {
-                        !posts.length > 0 ? <div className="m-auto"><CircularProgress/></div> : 
-                        <>
-                            <Grid item xs={12} sm={4}>
-                                <Form/>
-                            </Grid>
-                            <Grid item xs={12} sm={8}>
-                                <Posts posts={posts}/>
-                            </Grid>
-                        </>
-                    }
-                    </Grid>
-                </Container>
-            </Grow>
+            <Home/>
         </Container>
     )
 }
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { Grow, Container, Grid } from "@mui/material";
+import { CircularProgress } from "@mui/material";
+
+import { fetchPostsApi } from "../../api";
+import Form from '../Form/Form'
+import Posts from '../Posts/Posts'
+
+export default () => {
+    const dispatch = useDispatch();
+    const posts = useSelector(state => state.posts.items)
+
+    useEffect(() => {
+        dispatch(fetchPostsApi())
+    }, [dispatch])
+
+    return (
+        <Grow in>
+            <Container>
+                <Grid minHeight={600} container justifyItems="space-between" alignItems="stretch" spacing={3}>
+                {
+                    !posts.length > 0 ? <div className="m-auto"><CircularProgress/></div> : 
+                    <>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                        <Grid item xs={12} sm={8}>
+                            <Posts posts={posts}/>
+                        </Grid>
+                    </>
+                }
+                </Grid>
+            </Container>
+        </Grow>
+    )
+}
