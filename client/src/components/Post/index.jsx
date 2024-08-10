7f68b0a87fbbbff259beb23ebb19483677defa32
Ian Tumulak <edden87@gmail.com>
2024-08-10 20:26:28 +0800

    Improved structure.

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -1,7 +1,7 @@
 
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { Card, CardActions, CardMedia, Button, Typography, CardContent } from "@mui/material";
+import { Card, CardActions, CardMedia, Button, Typography, CardContent, Paper } from "@mui/material";
 import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from '@mui/icons-material/Edit';
@@ -20,25 +20,26 @@ export default ({post, onDelete}) => {
     return (
         <Card className="flex flex-col justify-between rounded-2xl h-full relative">
             <CardMedia className="h-48 pt-16 bg-blend-darken" image={selectedFile} title={title}>
-                <div className="absolute top-5 left-5 text-gray-800">
-                    <Typography variant="h6">{creator}</Typography>
-                    <Typography variant="body2">{formatDate(createdAt)}</Typography>
-                </div>
-                <div className="absolute top-5 right-5 text-white">
+                <div className="absolute top-5 right-1">
                     <Button className="text-white" size="small" onClick={() => navigate(`/edit/${id}`)}>
-                        <EditIcon className="text-white"/>
+                        <Paper className="p-1">
+                            <EditIcon fontSize="small" style={{color: 'black'}} className="text-white"/>    
+                        </Paper>
                     </Button>
                 </div>
-            </CardMedia>            
-            <div className="flex justify-between m-5">
-                <Typography variant="body2" color="textSecondary">{tags}</Typography>
+            </CardMedia>
+            <div className="pt-6 px-4">
+                <Typography variant="h5" gutterBottom>{title}</Typography>
+                <div className="flex flex-row gap-x-1 items-center">
+                    <Typography variant="body2">{creator}</Typography> - <Typography variant="body2">{formatDate(createdAt)}</Typography>
+                </div>
             </div>
-            <Typography className="py-0 px-4" variant="h5" gutterBottom>{title}</Typography>
             <CardContent>
-                <Typography className="p-0" variant="body2" gutterBottom>{message}</Typography>
+                <Typography className="p-0" variant="body1" gutterBottom>{message}</Typography>
+                <Typography variant="body2" color="textSecondary" className="flex gap-1">{tags.map(tag => <span>#{tag}</span>)}</Typography>
             </CardContent>
             <CardActions className="flex justify-between pt-0 pb-2 px-4">
-                <Button size="small" color="primary" onClick={handleLikePost}>
+                <Button className="flex flex-row items-center gap-x-1" size="small" color="primary" onClick={handleLikePost}>
                     <ThumbUpAltIcon fontSize="small"/> Like {likeCount}
                 </Button>
                 <Button size="small" color="primary" onClick={(event) => onDelete(event, id)}>
