4f740c0d9f63b926419ab7a183f51f2bd3125d17
Ian Tumulak <edden87@gmail.com>
2024-08-10 21:37:16 +0800

    More credentials to .env.

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
 import postRoutes from "./routes/postsRouter.js";
 
 const app = express();
-
+dotenv.config()
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
@@ -13,10 +15,9 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://admin:HBJQk4bQecp2PmHa@cluster0.aiukkji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(CONNECTION_URL)
+    .connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error))
