df7c6dd2aeb673cf547cab191883f900f2300d0c
Ian Tumulak <edden87@gmail.com>
2024-08-20 16:02:29 +0800

    Updated API url for client.

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -12,7 +12,7 @@ const CREATE_USER = 'user/create'
 const UPDATE_USER = 'user/update'
 const SIGNIN_USER = 'user/signin'
 const VALIDATE_USER = 'user/validate'
-const API = axios.create({baseURL: 'http://localhost:5000'})
+const API = axios.create({baseURL: 'https://memories-mern-stack-server.vercel.app'})
 
 API.interceptors.request.use((request) => {
     const token = localStorage.getItem('loginToken')
