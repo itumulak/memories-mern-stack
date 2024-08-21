918423f0dd2e41dce07f063809e6ee4c5330dc4b
Ian Tumulak <edden87@gmail.com>
2024-08-22 00:16:57 +0800

    Fix an issue where edit and delete shows while not logged in.

--- client/src/components/PageActions/PageActions.jsx
+++ client/src/components/PageActions/PageActions.jsx
@@ -4,13 +4,14 @@ import { Dialog, DialogContent, DialogContentText, DialogActions, Button, Button
 import CancelIcon from '@mui/icons-material/Cancel';
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { deletePostApi } from "../../api";
 
 export default ({id, submitting, onUpdate, edit}) => {
     const [isOpenDeleteDialog, setIsOpenDeleteDialog] = useState(false)
     const [editMode, setEditMode] = useState(edit || false)
+    const user = useSelector(state => state.auth)
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
@@ -44,22 +45,31 @@ export default ({id, submitting, onUpdate, edit}) => {
 
     return (
         <>
-            <ButtonBase onClick={(e) => handleDelete(e, id)}>
-                <DeleteIcon color="error"/>
-            </ButtonBase>
-            {editMode ?
-                <ButtonGroup variant="contained">
-                    <Button variant={submitting ? `outlined` : `contained`} disabled={submitting} size="small" onClick={onUpdate}>{submitting ? `Saving...` : `Save`}</Button>
-                    <Button size="small" color="error" onClick={handleCancelEdit}>
-                        <CancelIcon/>
-                    </Button>
-                </ButtonGroup> :
-                <ButtonBase 
-                    className="!py-4 !text-blue-600 !hover:text-blue-800 !visited:text-blue-600"
-                    onClick={handleEdit}
-                    variant="text"
-                >Edit</ButtonBase>
-            }
+            {user.isLogin && (
+                <>
+                    <ButtonBase onClick={(e) => handleDelete(e, id)}>
+                        <DeleteIcon color="error" />
+                    </ButtonBase>
+                    {editMode ? (
+                        <ButtonGroup variant="contained">
+                            <Button variant={submitting ? 'outlined' : 'contained'} disabled={submitting} size="small" onClick={onUpdate}>
+                                {submitting ? 'Saving...' : 'Save'}
+                            </Button>
+                            <Button size="small" color="error" onClick={handleCancelEdit}>
+                                <CancelIcon />
+                            </Button>
+                        </ButtonGroup>
+                    ) : (
+                        <ButtonBase 
+                            className="!py-4 !text-blue-600 !hover:text-blue-800 !visited:text-blue-600"
+                            onClick={handleEdit}
+                            variant="text"
+                        >
+                            Edit
+                        </ButtonBase>
+                    )}
+                </>
+            )}
             {createPortal(
                 <Dialog open={isOpenDeleteDialog} onClose={handleCloseDialog} aria-labelledby="confirm-delete-title" aria-describedby="confirm-delete-description">
                     <DialogTitle id="confirm-delete-title">Confirm Delete</DialogTitle>
